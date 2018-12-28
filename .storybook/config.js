d69a94ad9d8c43937227c715a22e33c03996f2e4
David Marín <ddmarin94@gmail.com>
2018-12-28 15:32:20 +0100

    added addon-action, addon-info, addon-knobs, addon-links, addon-notes, addont-options

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,28 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withOptions } from "@storybook/addon-options";
+import { withInfo } from "@storybook/addon-info";
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(withNotes);
+addDecorator(withKnobs);
+addDecorator(withInfo)
+
+withOptions({
+  name: "Storybook",
+  url: "https://test.com",
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: true,
+  sortStoriesByKind: true
+});
 
 configure(loadStories, module);

1aca0ed38ddbe5cbdc75b81e923e5f76ceec22fc
David Marín <ddmarin94@gmail.com>
2018-12-29 10:41:21 +0100

    feat(addon): added viewport addon and also modified example component

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './index.scss'
 
 export const Button = ({children, action, disabled}) => (
-  <button className={`button${disabled ? '--disabled' : ''}`} onClick={action}>
+  <button disabled={disabled} className={`${disabled ? 'button--disabled' : 'button'}`} onClick={action}>
     {children}
   </button>
 )
+
+Button.propTypes = {
+  /** Text that will receive the button component  */
+  children: PropTypes.element.isRequired,
+  /** Will be executed when button is clicked */
+  action: PropTypes.func.isRequired,
+  /** Enable/disables the botton */
+  disabled: PropTypes.bool.isRequired
+}
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,5 +8,13 @@ import {Button} from '../packages/button/src'
 import {text, boolean, number } from '@storybook/addon-knobs';
 
 storiesOf('Button', module)
-  .add('Basic', () => <Button disabled={boolean('disabled', false)} action={action('clicked')} >Hello world</Button>,  { notes: 'A very simple component' })
+  .add('Basic', 
+  () => (
+      <Button 
+        disabled={boolean('disabled', false)} 
+        action={action('clicked')} >
+          Hello world
+      </Button>), 
+      { notes: 'A very simple component' }
+    )
 
